1b0902bc960de8ccc25fede8c685a7b71d20e82c
relu00123 <107416418+relu00123@users.noreply.github.com>
2023-05-11 16:28:21 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int number;
+
+	std::cin >> number;
+
+	for (int i = number; i >= 1; i--)
+	{
+		std::cout << i << "\n";
+	}
+
+	std::cout << endl;
+
+	return 0;
+}
