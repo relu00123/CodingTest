c64b518ba5064979c0cccc684ba94ac471ac260f
relu00123 <107416418+relu00123@users.noreply.github.com>
2023-05-18 18:15:58 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 120 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool compare(const pair<int, int>&  _First, const pair<int, int>& _Second)
+{
+	 //y좌표가 같지 않은경우 먼저 정렬의 기준이 된다.
+	if (_First.second != _Second.second)
+	{
+		return _First.second < _Second.second;
+	}
+
+	// y좌표가 같을시
+	else
+	{
+		return _First.first < _Second.first;
+	}
+
+}
+
+
+int main()
+{
+	int N;
+	int x, y;
+	std::cin >> N;
+
+	vector<pair<int, int>> Buffer;
+
+	for (int i = 1; i <= N; ++i)
+	{
+		std::cin >> x >> y;
+
+		Buffer.push_back(make_pair(x, y));
+	}
+
+	sort(Buffer.begin(), Buffer.end(), compare);
+
+	for (int i = 0; i < N; ++i)
+	{
+		std::cout << Buffer[i].first << " " << Buffer[i].second << "\n";
+	}
+
+	return 0;
+}
