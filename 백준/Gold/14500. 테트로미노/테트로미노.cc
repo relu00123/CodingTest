34994a9c9435d19233b841edda3202504b6e6287
relu00123 <107416418+relu00123@users.noreply.github.com>
2023-11-13 00:43:12 +0900

    [Gold IV] Title: 테트로미노, Time: 36 ms, Memory: 3016 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -1,92 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define X first
+#define Y second
 
+// 테트로미노의 모든 조합
+pair<int, int> tetro[19][4] = {
+    // 청록색 블록
+    {{0, 0}, {0, 1}, {0, 2}, {0, 3}},
+    {{0, 0}, {1, 0}, {2, 0}, {3, 0}},
+    // 노랑색 블록
+    {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
+    // 주황색 블록
+    {{0, 0}, {0, 1}, {0, 2}, {1, 2}},
+    {{0, 0}, {0, 1}, {0, 2}, {1, 0}},
+    {{1, 0}, {1, 1}, {1, 2}, {0, 2}},
+    {{1, 0}, {1, 1}, {1, 2}, {0, 0}},
 
-int N, M;
-int graph[500][500];
-int vis[500][500];
+    {{0, 1}, {1, 1}, {2, 1}, {0, 0}},
+    {{0, 1}, {1, 1}, {2, 1}, {2, 0}},
+    {{0, 0}, {1, 0}, {2, 0}, {0, 1}},
+    {{0, 0}, {1, 0}, {2, 0}, {2, 1}},
+    // 초록색 블록
+    {{0, 0}, {0, 1}, {1, 1}, {1, 2}},
+    {{1, 0}, {1, 1}, {0, 1}, {0, 2}},
+    {{0, 0}, {1, 0}, {1, 1}, {2, 1}},
+    {{0, 1}, {1, 1}, {1, 0}, {2, 0}},
+    // 자주색 블록
+    {{0, 0}, {0, 1}, {0, 2}, {1, 1}},
+    {{1, 0}, {1, 1}, {1, 2}, {0, 1}},
+    {{0, 0}, {1, 0}, {2, 0}, {1, 1}},
+    {{0, 1}, {1, 1}, {2, 1}, {1, 0}}};
+int board[505][505];
+int n, m;
 
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
- 
-int maxcount = 0;
-
-void func(int _stack, int _xpos, int _ypos, int _sum) {
-
-	if (_stack == 4) {
-		maxcount = max(maxcount, _sum);
-		return;
-	}
-
-
-	if (_xpos < 0 || _xpos >= N || _ypos < 0 || _ypos >= M)
-		return;
-
-
-	int cursum = _sum + graph[_xpos][_ypos];
-		
-
-	vis[_xpos][_ypos] = true;
-
-	for (int i = 0; i < 4; ++i) {
-		int nx = _xpos + dx[i];
-		int ny = _ypos + dy[i];
-
-		if (vis[nx][ny] == false) {
-		
-			func(_stack + 1, nx, ny, cursum);
-		}
-	}
-
-	vis[_xpos][_ypos] = false;
+int bruteforce(int x, int y) {
+  int mx = 0;
+  for (int i = 0; i < 19; i++) {
+    int sum = 0;
+    for (int j = 0; j < 4; j++)
+      sum += board[x + tetro[i][j].X][y + tetro[i][j].Y];
+    mx = max(mx, sum);
+  }
+  return mx;
 }
 
-void specialcase(int _xpos, int _ypos) {
-	int support[4] = { 0, 0, 0, 1 };
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
+  cin >> n >> m;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      cin >> board[i][j];
 
-	do {
-		int semisum = 0;
-		semisum += graph[_xpos][_ypos];
-
-		for (int i = 0; i < 4; ++i) {
-			if (support[i] == 0) {
-				int nx = _xpos + dx[i];
-				int ny = _ypos + dy[i];
-
-				if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-				semisum += graph[nx][ny];
-
-			}
-		}
-
-		maxcount = max(semisum, maxcount);
-
-	} while (next_permutation(support, support + 4));
-
-}
-
-
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> N >> M;
-
-	// 입력 받기
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) {
-			cin >> graph[i][j];
-		}
-	}
-
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) {
-			func(0, i, j, 0);
-			specialcase(i, j);
-		}
-	}
-
-	cout << maxcount << "\n";
-	return 0;
+  int ans = 0;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      ans = max(ans, bruteforce(i, j));
+  cout << ans;
 }
