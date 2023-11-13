4ba223d482ec0dbcde4ad8c5be5fcfe89dcf5669
relu00123 <107416418+relu00123@users.noreply.github.com>
2023-11-13 17:22:12 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -2,56 +2,36 @@
 using namespace std;
 
 int n;
-int final_small = 1e9;
-int final_big = -1e9;
+int num[102];
+int cal[4];
+int mx = -0x7f7f7f7f;
+int mn = 0x7f7f7f7f;
 
-int main() {
+void func(int ans, int k) {
+	if (k == n) {
+		mx = max(mx, ans);
+		mn = min(mn, ans);
+		return;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		if (!cal[i]) continue;
+		cal[i]--;
+		if (i == 0) func(ans + num[k], k + 1);
+		else if (i == 1) func(ans - num[k], k + 1);
+		else if (i == 2) func(ans * num[k], k + 1);
+		else if (i == 3) func(ans / num[k], k + 1);
+		cal[i]++;
+	}
+}
+
+int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin >> n;
-
-	vector<int> arr(n);
-	for (int i = 0; i < arr.size(); ++i)
-		cin >> arr[i];
-
-	// 덧샘, 뺄셈, 곱셈, 나눗셈의 개수가 주어짐
-	vector<int> opp; 
-
-	for (int i = 0; i < 4; ++i) {
-		int temp;
-		cin >> temp;
-		while (temp--)
-			opp.push_back(i);
-	}
-
-	do {
-		int cur_num = arr[0];
-
-		for (int i = 0; i < arr.size() - 1; ++i) {
-			if (opp[i] == 0) { // 덧셈의 경우
-				cur_num = cur_num + arr[i+1];
-			}
-			else if (opp[i] == 1) { // 뺄셈의 경우
-				cur_num = cur_num - arr[i+1];
-			}
-			else if (opp[i] == 2) { // 곱셈의 경우
-				cur_num = cur_num * arr[i+1];
-			}
-			else if (opp[i] == 3) { // 나눗셈의 경우
-				cur_num = cur_num / arr[i+1];
-			}
-		}
-
-		final_small = min(final_small, cur_num);
-		final_big = max(final_big, cur_num);
-
-
-	} while (next_permutation(opp.begin(), opp.end()));
-
-
-	cout << final_big << "\n";
-	cout << final_small << "\n";
-
-	return 0;
+	for (int i = 0; i < n; i++) cin >> num[i];
+	for (int i = 0; i < 4; i++) cin >> cal[i];
+	func(num[0], 1);
+	cout << mx << "\n" << mn << '\n';
 }
