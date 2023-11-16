a555c9f1ad257fc5c61875aa8b508d928349f6f3
relu00123 <107416418+relu00123@users.noreply.github.com>
2023-11-17 02:20:16 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 4012 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,55 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> arr;
 int n;
+int a[505][505], d[505][505];
 
-int dp[500][500];
-
-int main() {
+int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
 	cin >> n;
-
-	// 입력 받기
-	for (int i = 0; i < n; ++i) {
-		vector<int> newarr;
-		
-		for (int j = i; j >= 0; j--) {
-			int temp;
-			cin >> temp;
-			newarr.push_back(temp);
-
-			if (i == 0) {
-				dp[0][0] = temp;
-			}
-
-		}
-
-		arr.push_back(newarr);
-	}
-
-
-	for (int i = 1; i < n; ++i) {
-		for (int j = 0; j <= i; ++j) {
-			
-			// 가장 자리인 경우 선택권이 없음
-			if (j == 0) {
-				dp[i][j] = dp[i - 1][0] + arr[i][j];
-			}
-			else if (j == i ) {
-				dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
-			}	
-
-			else {
-				dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
-			}
-		}
-	}
-
-
-	auto ans = max_element(dp[n - 1], dp[n-1] + n);
-
-	cout << *ans << "\n";
-
+	for (int i = 1; i <= n; ++i)
+		for (int j = 1; j <= i; ++j)
+			cin >> a[i][j];
+	d[1][1] = a[1][1];
+	for (int i = 2; i <= n; ++i)
+		for (int j = 1; j <= i; ++j)
+			d[i][j] = max(d[i - 1][j - 1], d[i - 1][j]) + a[i][j];
+	cout << *max_element(d[n] + 1, d[n] + n + 1);
 }
