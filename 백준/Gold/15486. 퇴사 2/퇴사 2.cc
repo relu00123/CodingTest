ce67b228cdf1570dbe339305d0f631b66046112c
relu00123 <107416418+relu00123@users.noreply.github.com>
2023-11-18 12:02:33 +0900

    [Gold V] Title: 퇴사 2, Time: 220 ms, Memory: 19600 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int N;
+
+int arr[2][1500002];
+int dp[1500002];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+	for (int i = 1; i <= N; ++i) {
+		cin >> arr[0][i] >> arr[1][i];
+	}
+
+	for (int i = N; i >= 1; i--) {
+
+		// 임무를 받지 못하는 경우
+		if (arr[0][i] + i > N + 1)
+			dp[i] = dp[i + 1];
+
+		// 임무를 받을 수 있는 경우 
+		else
+			dp[i] = max(arr[1][i] + dp[i + arr[0][i]], dp[i + 1]);
+		 
+	}
+
+	//for (int i = 0; i <= N; ++i)
+	//	cout << dp[i] << " ";
+
+	cout << dp[1] << "\n";
+
+	return 0;
+}
