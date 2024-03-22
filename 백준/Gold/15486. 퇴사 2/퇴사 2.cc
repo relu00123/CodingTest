dd59bb61bbfcec395ce195322482cd4effea873d
relu00123 <107416418+relu00123@users.noreply.github.com>
2024-03-23 04:02:00 +0900

    [Gold V] Title: 퇴사 2, Time: 216 ms, Memory: 19600 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -1,36 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int N;
-
-int arr[2][1500002];
-int dp[1500002];
-
-int main() {
+int main()
+{
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> N;
-	for (int i = 1; i <= N; ++i) {
-		cin >> arr[0][i] >> arr[1][i];
-	}
+	int n; cin >> n;
 
-	for (int i = N; i >= 1; i--) {
+	// 0번째 인덱스에는 걸리는 시간, 1번째 인덱스에는 보수를 저장
+	vector<int> time(n + 1);
+	vector<int> cost(n + 1);
 
-		// 임무를 받지 못하는 경우
-		if (arr[0][i] + i > N + 1)
-			dp[i] = dp[i + 1];
+	// n일차에 받는 최대 보수
+	vector<int> dp(n + 1);
+	fill(dp.begin(), dp.end(), 0);
 
-		// 임무를 받을 수 있는 경우 
-		else
-			dp[i] = max(arr[1][i] + dp[i + arr[0][i]], dp[i + 1]);
+	for (int i = 1; i <= n; ++i)
+		cin >> time[i] >> cost[i];
+
+	dp[0] = 0;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		dp[i] = max(dp[i], dp[i - 1]);
+
+		if (i + time[i] - 1 > n) continue;
+		
 		 
+		dp[i + time[i] -1 ] = max(dp[i - 1] + cost[i], dp[i + time[i] - 1]);
+
+		/*cout << i << ": ";
+		for (int i = 1; i <= n; ++i)
+		{
+			cout << dp[i] << " ";
+		}
+		cout << "\n";*/
+
 	}
 
-	//for (int i = 0; i <= N; ++i)
-	//	cout << dp[i] << " ";
+	/*for (int i = 1; i <= n; ++i)
+	{
+		cout << dp[i] << " ";
+	}*/
 
-	cout << dp[1] << "\n";
+	cout << dp[n] << "\n";
+
 
 	return 0;
 }
