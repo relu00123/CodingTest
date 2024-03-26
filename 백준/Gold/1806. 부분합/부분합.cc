e802acf6cab9847f84352ab9ea09a00ba3a0d75d
relu00123 <107416418+relu00123@users.noreply.github.com>
2024-03-26 11:40:54 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -1,53 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int n, s, tot;
+int a[100005];
+int mn = 0x7fffffff;
+
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-
-	// 10'000 이하의 수들로 구성된 길이 N짜리 수열이 주어짐
-	// 수열에서 연속된 수들의 부분합중에서 그 합이 S이상이 되는 것 중 가장 짧은 것의 길이를 
-	// 구하는 프로그램을 작성하시오 
-
-
-
-	int ans = 200'000'000;
-
-	int n, s; cin >> n >> s;
-	vector<int> a(n); 
-	for (int i = 0 ; i < n; ++i)
+	cin >> n >> s;
+	for (int i = 0; i < n; i++)
 		cin >> a[i];
+	tot = a[0];
+	int en = 0;
 
-	 
-
-	int st = 0, en = 0;
-	int sum = a[0];
-	if (sum >= s) {
-		cout << 1 << "\n";
-		return 0 ;
-	}
-
-	while (st < n && en < n) {
-		if (sum < s) {
-			en++; 
-			if (en == n)
-				break;
-			sum += a[en];
+	for (int st = 0; st < n; st++) {
+		while (en < n && tot < s) {
+			en++;
+			if (en != n)tot += a[en];
 		}
-		else {
-			ans = min(ans, en - st + 1);
-			sum -= a[st];
-			st++;
-			 
-		}
+		if (en == n) break; // en이 범위를 벗어날시 종료
+		mn = min(mn, en - st + 1);
+		tot -= a[st];
 	}
-
-	if (ans == 200'000'000)
-		cout << "0";
-	else
-		cout << ans;
-
-	return 0;
+	if (mn == 0x7fffffff) mn = 0;
+	cout << mn;
 }
