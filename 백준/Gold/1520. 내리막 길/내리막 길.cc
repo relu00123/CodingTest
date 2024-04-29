ed67e11abd70a77f4ac439eb704972c180d94ac3
relu00123 <107416418+relu00123@users.noreply.github.com>
2024-04-30 02:43:48 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 4012 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -1,58 +1,33 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[500][500];
-int arr[500][500];
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
 int n, m;
+int a[505][505], d[505][505];
 
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
-
-int calc(int _curx, int _cury) {
-
-	if (_curx == n - 1 && _cury == m - 1)
-		return 1;
-
-	if (dp[_curx][_cury] == -1) { // 초행인 경우
-
-		int return_value = 0;
-
-		for (int dir = 0; dir < 4; ++dir) {
-			int nx = _curx + dx[dir];
-			int ny = _cury + dy[dir];
-
-			if (nx < 0 || nx >= n || ny <0 || ny >= m) continue; // OOB
-
-			if (arr[_curx][_cury] <= arr[nx][ny]) continue;
-
-			return_value += calc(nx, ny);
-		}
-
-		dp[_curx][_cury] = return_value;
-		return dp[_curx][_cury];
-	}
-
-	else { // dfs로 인해 채워진 경우
-		return dp[_curx][_cury];
-	}
-
+int func(int x, int y){
+  if(d[x][y] != -1) return d[x][y];
+  if(x == n - 1 && y == m - 1) return 1;
+  int &ret = d[x][y];
+  ret = 0;
+  for(int dir = 0; dir < 4; ++dir){
+    int nx = x + dx[dir];
+    int ny = y + dy[dir];
+    if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+    if(a[x][y] > a[nx][ny]) ret += func(nx, ny);
+  }
+  return ret;
 }
 
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	// 내릭막길로만 이동하는 경로의 개수를 구하는 프로그램을 작성하시오
-	for (int i = 0; i < 500; ++i) 
-		fill(dp[i], dp[i] + 500, -1);
-
-	cin >> n >> m;
-
-	for (int i = 0; i < n; ++i) 
-		for (int j = 0; j < m; ++j)
-			cin >> arr[i][j];
-	
-	cout << calc(0, 0) << "\n";
-	return 0;
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  cin >> n >> m;
+  for(int i = 0; i < n; ++i)
+    for(int j = 0; j < m; ++j)
+      cin >> a[i][j], d[i][j] = -1;
+  cout << func(0, 0);
 }
