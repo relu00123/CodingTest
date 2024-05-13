97bf87d465cb30225a2eac8878ce529ec8718beb
relu00123 <107416418+relu00123@users.noreply.github.com>
2024-05-14 05:13:05 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
@@ -1,174 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int max_num = 1e9;
+int cnt = 0;
 
-
-struct info
-{
-	int  productnum;
-	bool appeared;
-	int  nextappearindex;
-
-
-	bool operator<(const info _other) const
-	{
-		if (this->appeared == true && _other.appeared == false)
-		{
-			return false;
-		}
-
-		else
-		{
-			return this->nextappearindex > _other.nextappearindex;
-		}
-	}
-};
-
-
-
-
- 
-int main()
-{
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n, k;
 
-	cin >> n;     // n이 멀티탭 개수
-	cin >> k;     // k가 제품
+	int n, k; cin >> n >> k;
 
+	vector<int> infos(k);
 
-	if (k <= n) {
-		cout << "0";
-		return 0;
-	}
+	for (int i = 0; i < k; ++i) 
+		cin >> infos[i];
 
-	vector<info> checked;
-	vector<int> arr(k);
+	unordered_map<int, int> tabs;
 	 
+	for (int i = 0; i < k; ++i) {  // 100개 
 
-	bool shoulddelete;
-
-	int plug_count = 0;
-
-	for (int i = 0; i < k; ++i)
-	{
-		cin >> arr[i]; 
-	}
-
-
-	for (int i = 0; i < k; ++i)
-	{
-		if (checked.size() < n)
-		{
-			bool isSameDataExisted = false;
-
-			for (int a = 0; a < checked.size(); ++a)
-			{
-				if (checked[a].productnum == arr[i])
-					isSameDataExisted = true;
-			}
-			
-			if (!isSameDataExisted)
-			{
-				info data;
-				data.productnum = arr[i];
-				data.appeared = false;
-				data.nextappearindex = 2000;
-				checked.push_back(data);
-			} 
+		if (tabs.size() < n) {
+			tabs[infos[i]] = max_num;
 		}
 
-		else
-		{
-			int passed = true;
-			shoulddelete = true;
+		else {
+			// 현재 꽂은 걸로 해결할 수 있는지 확인.
+			if (tabs.find(infos[i]) != tabs.end()) continue;
 
-			while (passed)
+
+			// 어떤 것이 가장 마지막에 나오는지 찾아야함. 
+			for (int j = i + 1; j < k; ++j) // 100개 
 			{
-				for (int j = 0; j < checked.size(); ++j)
-				{
-					if (checked[j].productnum == arr[i])
-					{
-
-						shoulddelete = false;
-						if (checked[j].appeared == false)
-						{
-							checked[j].appeared = true;
-							checked[j].nextappearindex = i;
-						}
-					}
-
-
-					else
-					{
-						passed = false;
-						 
-					}
-				}
-
-				passed = false;
+				auto iter = tabs.find(infos[j]);
+				if (iter == tabs.end() || iter->second < j) continue;
+				iter->second = j;
 			}
 
-			// 가장 최근에 발생한 위치를 기록해야한다.
-			for (int j = i; j < k; ++j)
+			int temp_num = 0;
+			int target_num = -1;
+
+			// tabs의 값이 가장 큰애를 기억했다가 멀티탭에서 뺀다. 
+			// 그리고 tab에 infos[i]를 추가한다.
+			for (auto iter = tabs.begin(); iter != tabs.end(); ++iter)
 			{
-				for (int l = 0; l < checked.size(); ++l)
+				if (iter->second > temp_num)
 				{
-					if (arr[j] == checked[l].productnum && checked[l].appeared == false)
-					{
-						checked[l].appeared = true;
-						checked[l].nextappearindex = j;
-					}
+					temp_num = iter->second;
+					target_num = iter->first;
 				}
 			}
 
+			tabs.erase(target_num);
+			cnt += 1;
 
-			// sorting 작업 진행해야함. 멀티탭에서 사출해야할 순서로
-			if (shoulddelete == true)
-			{
-				sort(checked.begin(), checked.end());
-
-				plug_count += 1;
-				checked.erase(checked.begin());
-
-
-				if (checked.size() < n)
-				{
-					 
-						info data;
-						data.productnum = arr[i];
-						data.appeared = false;
-						data.nextappearindex = 2000;
-						checked.push_back(data);
-				 
-				}
-
+			for (auto iter = tabs.begin(); iter != tabs.end(); ++iter) {
+				iter->second = max_num;
 			}
-		}
 
-
-
-
-		/*cout << "index : " << i << " and current pluged : ";
-		for (int i = 0; i < checked.size(); ++i)
-		{
-			cout << checked[i].productnum << " ";
-		}
-		cout << "\n";*/
-
-		for (int i = 0; i < checked.size(); ++i)
-		{
-			checked[i].appeared = false;
-			checked[i].nextappearindex = 2000;
-
+			tabs[infos[i]] = max_num;
 		}
 
 	}
 
-	cout << plug_count;
-
-	return 0;
+	cout << cnt << "\n";
+	
 
 }
