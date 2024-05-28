09ff67ca89648d8fdcfe54b91f5eb6a7aee4706f
relu00123 <107416418+relu00123@users.noreply.github.com>
2024-05-28 11:48:11 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1440 ms, Memory: 43588 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -2,31 +2,34 @@
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
+  int n, m;
+  deque<pair<int, int>> dq;
 
-	int n, l; cin >> n >> l;
+  cin >> n >> m;
 
-	// A1 A2 ... An까지의 수가 주어진다.
+  for (int i = 0; i < n; ++i) {
+    int num;
+    cin >> num;
 
-	// 값, 들어온 index
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    // 새로운 숫자가 deque내에서 가장 큰 값의 숫자이고 마지막에 위치하도록 합니다.
+    // deque안의의 숫자들은 오름차순 상태를 유지하게 되고, 구간내 가장 작은값은 항상 맨앞(front)에 있는 상태를 가지게 됩니다.
+    while (!dq.empty() && dq.back().second >= num) {
+      dq.pop_back();
+    }
+    
+    // 새로운 숫자는 항상 삽입됩니다.
+    dq.push_back({i, num});
 
-	for (int idx = 1; idx <= n; idx++) {
-		int temp; cin >> temp;
-		pq.push({ temp, idx });
+    // 주어진 구간을 벗어나는 outdate된 숫자들은 제거해줍니다.
+    if (dq.front().first <= i - m) {
+      dq.pop_front();
+    }
 
-		while (pq.top().second < idx - l + 1) {
-			pq.pop();
-		}
+    cout << dq.front().second << " ";
+  }
 
-		if (!pq.empty())
-			cout << pq.top().first << " ";
-		else
-			cout << "Something Wrong\n";
-	}
-
-
-	return 0;
+  return 0;
 }
