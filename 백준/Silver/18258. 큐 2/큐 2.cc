797f86890158e5a40e63b6055d77e64ae7d87fb3
relu00123 <107416418+relu00123@users.noreply.github.com>
2024-10-03 15:44:28 +0900

    [Silver IV] Title: 큐 2, Time: 360 ms, Memory: 10424 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.cc
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -3,69 +3,67 @@ using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(0);
 	cin.tie(0);
+	ios::sync_with_stdio(0);
+
 
 	int n; cin >> n;
 
-	deque<int> dq;
+	queue<int> q;
 
 	while (n--)
 	{
-		string order; cin >> order;
+		string str; cin >> str;
 
-		if (order == "push")
+
+		if (str == "front")
+		{
+			if (q.size() == 0)
+				cout << "-1\n";
+			else
+				cout << q.front() << "\n";
+		}
+
+		else if (str == "back")
+		{
+			if (q.size() == 0)
+				cout << "-1\n";
+			else
+				cout << q.back() << "\n";
+		}
+
+		else if (str == "size")
+		{
+			cout << q.size() << "\n";
+		}
+
+		else if (str == "empty")
+		{
+			if (q.size() == 0)
+				cout << "1\n";
+			else
+				cout << "0\n";
+		}
+
+		else if (str == "pop")
+		{
+			if (q.size() == 0)
+				cout << "-1\n";
+			else
+			{
+				cout << q.front() << "\n";
+				q.pop();
+			}
+		}
+
+		else if (str == "push")
 		{
 			int temp; cin >> temp;
-			dq.push_back(temp);
+			q.push(temp);
 		}
 
-		else if (order == "pop")
-		{
-			if (dq.size() != 0)
-			{
-				cout << dq.front() << "\n";
-				dq.pop_front();
-			}
-			else
-			{
-				cout << "-1\n";
-			}
-		}
+		
 
-		else if (order == "size")
-		{
-			cout << dq.size() << "\n";
-		}
-
-		else if (order == "empty")
-		{
-			if (dq.empty())
-			{
-				cout << "1\n";
-			}
-			else
-			{
-				cout << "0\n";
-			}
-		}
-
-		else if (order == "front")
-		{
-			if (dq.empty())
-				cout << "-1\n";
-			else
-				cout << dq.front() << "\n";
-		}
-
-		else if (order == "back")
-		{
-			if (dq.empty())
-				cout << "-1\n";
-			else
-				cout << dq.back() << "\n";
-		}
 	}
 
-	return 0;
 }
