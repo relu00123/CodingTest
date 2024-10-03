1ef265a7578e43b41930d608ff48670e0978073f
relu00123 <107416418+relu00123@users.noreply.github.com>
2024-10-03 12:35:51 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 80 ms, Memory: 2284 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -3,53 +3,56 @@ using namespace std;
 
 int main()
 {
-	int n, k;
-	cin >> n >> k;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-	list<int> arr = {};
+
+	int n, k; cin >> n >> k;
+
+	list<int> arr;
+
+
 	for (int i = 1; i <= n; ++i)
+	{
 		arr.push_back(i);
+	}
+
+ 
 
 	list<int>::iterator iter = arr.begin();
 
 	std::cout << "<";
 
 	while (arr.size() != 0)
-	{ 
-
-		for (int i = 1; i < k; ++i)
+	{
+	
+		for (int i = 0; i < k - 1; ++i)
 		{
- 
-			iter++;
-
-			if (iter == arr.end())
+			// iterator를 증가시킨다.
+			if (next(iter) == arr.end())
 			{
-			 
-
 				iter = arr.begin();
 			}
-				 
+
+			else
+			{
+				iter++;
+			}
 		}
 
-		n--;
 
-		if (n != 0)
-
-			cout << *iter << ", ";
-
+		// 원소를 삭제한다.
+		if (arr.size() != 1)
+			std::cout << *iter << ", ";
 		else
-			cout << *iter;
-		 
+			std::cout << *iter;
 		iter = arr.erase(iter);
 
 		if (iter == arr.end())
 			iter = arr.begin();
-
- 
 	}
 
-	cout << ">";
+	std::cout << ">";
 
-
-	return 0;
+	
 }
