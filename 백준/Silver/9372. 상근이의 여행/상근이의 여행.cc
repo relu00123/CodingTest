f118c57fc21e20ef7536ad715750ec249d004ad9
relu00123 <107416418+relu00123@users.noreply.github.com>
2024-10-24 00:45:11 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+
+	// 가장 적은 종류의 비행기를 타고 모든 국가 여행
+	// n개국을 여행할 것임
+
+	int tc; cin >> tc;
+	while (tc--) {
+		int n, m; cin >> n >> m;
+
+		for (int i = 0; i < m; ++i) {
+			int a, b; cin >> a >> b;  // a와 b를 연결하는 그래프가 있다. 
+
+		}
+
+		cout << n - 1 << "\n";
+	
+	}
+
+
+	return 0;
+}
